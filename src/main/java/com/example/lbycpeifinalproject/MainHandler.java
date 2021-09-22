4cbe4ba4285d213456abb809eb6e932b224672c9
Jericho Benitez <mrrenzjericho@gmail.com>
2021-09-22 22:58:58 +0800

    Updated BuyerMainController & MainHandler

--- src/main/java/com/example/lbycpeifinalproject/MainHandler.java
+++ src/main/java/com/example/lbycpeifinalproject/MainHandler.java
@@ -1,12 +1,9 @@
 package com.example.lbycpeifinalproject;
 
-import com.example.lbycpeifinalproject.misc.DatabaseController;
-import com.example.lbycpeifinalproject.misc.ProductObject;
-import com.example.lbycpeifinalproject.seller.EditProductApplication;
-import com.example.lbycpeifinalproject.seller.EditProductController;
+import com.example.lbycpeifinalproject.buyer.Login;
 
 public class MainHandler {
     public static void main(String[] args) throws Exception {
-        EditProductApplication.main(null);
+        Login.main(null);
     }
 }
